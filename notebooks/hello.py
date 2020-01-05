6d3888265de6b5da46005aaed3221a9fc6b70bfd
norville <ing.norville@gmail.com>
2020-01-05 21:37:01 +0100

    add test file

---
+++ notebooks/hello.py
@@ -0,0 +1,14 @@
+# hello.py
+from __future__ import print_function
+
+def main():
+    print( '''
+            _   _      _ _       _ 
+           | | | | ___| | | ___ | |
+           | |_| |/ _ \ | |/ _ \| |
+           |  _  |  __/ | | (_) |_|
+           |_| |_|\___|_|_|\___/(_)
+
+           ''' )
+
+main()
